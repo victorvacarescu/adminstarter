e7bfbb2dace4341a79b6c2e42d2a3b795d9024fc
Victor Vacarescu <victor.vacarescu@gmail.com>
2019-06-04 19:05:27 +0300

    test

---
+++ src/components/Login.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        Bine ai venit pe pagina de Login
+
+        <el-input placeholder="blaaa"></el-input>
+    </div>    
+</template>
+<script>
+export default {
+    name: "Login",
+    data () {
+        return {
+
+        }
+    },
+    methods:{
+
+    }
+ }
+</script>
